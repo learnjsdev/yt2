b8c11de6bdb57da15579a8849dab5ce3cb545f9b
i.melnyk <learn.js.dev@gmail.com>
2021-11-22 14:37:34 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -1,45 +1,56 @@
 class YTAudio {
   constructor() {
     this._init();
+    this._onPlayerReady = this._onPlayerReady.bind(this);
+    this._addEvents = this._addEvents.bind(this);
+    this._handleClick = this._handleClick.bind(this);
   }
 
+  _onPlayerReady(event) {
+    console.log('ready');
+    //event.target.playVideo();
+    //debugger
+  }
 
+  _onPlayerStateChange(event) {
+    console.log('state', event);
+  }
+
+  _handleClick(e) {
+    if (e.target.closest('.yt-player__toggle')) {
+      console.log(this._player);
+      this._player.playVideo();
+    }
+  }
+
+  _addEvents() {
+    document.addEventListener('click', this._handleClick.bind(this))
+  }
 
   _init() {
     const script = document.createElement('script');
     script.src = '//www.youtube.com/iframe_api';
     document.body.appendChild(script);
 
-    function onPlayerReady(event) {
-      console.log('ready')
-    }
-
-    function onPlayerStateChange(event) {
-      console.log('state', event);
-    }
-
-
     script.onload = () => {
-      console.dir(YT)
-
-
-      const player =  new YT.Player('#player', {
-          height: '200',
-          width: '800',
-          videoId: 'M7lc1UVf-VE',
+      window.YT.ready(() => {
+        this._player = new window.YT.Player("player", {
+          height: "390",
+          width: "640",
+          videoId: "M7lc1UVf-VE",
+          playerVars: {
+            //controls: 0
+          },
           events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            onReady: this._onPlayerReady,
+            onStateChange: this._onPlayerStateChange
           }
         });
-
-
-
+      });
     }
+
+    this._addEvents();
   }
 }
 
 new YTAudio();
-
-
-
